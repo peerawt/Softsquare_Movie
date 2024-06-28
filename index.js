ae4ea206f533f55d855611e39b3a20e5b5100bbd
peerawt <perawat1122kattakarn@gmail.com>
2024-06-28 17:37:04 +0700

    change every thing

---
+++ index.js
@@ -0,0 +1,4 @@
+const movie =[{namemovie:"movie1", description:"bsdshdgjhsgdgsj"}];
+document.getElementById('moviename1').innerHTML = movie[0].namemovie;
+document.getElementById('description1').innerHTML = movie[0].description;
+
